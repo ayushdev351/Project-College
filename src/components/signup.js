2f6f7775fcd8140b12c84161ca7d95358f9ad69a
ayushdev351 <79743704+ayushdev351@users.noreply.github.com>
2021-11-20 17:36:45 +0530

    Registeration Done

--- src/components/signup.js
+++ src/components/signup.js
@@ -1,4 +1,6 @@
 import React, { useRef } from "react";
+import styled from "styled-components";
+
 import { useUserContext } from "../context/userContext";
 
 const Signup = () => {
@@ -17,15 +19,73 @@ const Signup = () => {
 
   return (
     <div className="form">
-      <h2> New User</h2>
-      <form onSubmit={onSubmit}>
-        <input placeholder="Email" type="email" ref={emailRef} />
-        <input placeholder="Name" type="name" ref={nameRef} />
-        <input placeholder="Password" type="password" ref={psdRef} />
-        <button type="submit">Register</button>
-      </form>
+      <WebName>Welcome To Galgotia Connect</WebName>
+      <SignUp>
+        <h2> New User</h2>
+        <form onSubmit={onSubmit}>
+          <input placeholder="Email" type="email" ref={emailRef} />
+          <br />
+          <input placeholder="Name" type="name" ref={nameRef} />
+          <br />
+          <input placeholder="Password" type="password" ref={psdRef} />
+          <br />
+          <button type="submit">Register</button>
+        </form>
+      </SignUp>
     </div>
   );
 };
 
 export default Signup;
+
+const WebName = styled.div`
+  margin-top: 20px;
+  font-size: 40px;
+  font-family: cursive;
+`;
+
+const SignUp = styled.div`
+  margin-top: 7%;
+  padding: 20px;
+  margin-left: 30%;
+  margin-right: 30%;
+
+  border-radius: 20px;
+  background-color: #58d68d;
+  text-align: center;
+
+  >h2{
+     color : black;
+     padding-bottom: 20px;
+     font-size : 35px;
+     font-family: cursive;  
+   }
+    
+    >form{
+      >input{
+        color: black;
+        height: 25px;
+        width: 50%;
+        margin: 10px;
+        border-color: white;
+        border-radius: 10px;
+        }
+
+        >button{
+          padding: 9px;
+          margin: 15px;
+          width: 30%;
+          border-radius: 5px;
+          background-color: black;
+          font-size: 15px;
+          font-weight: bold;
+          font-family: cursive;
+          color: white;
+          :hover{
+            cursor: pointer;
+            color: blue;
+          }
+        }
+
+
+`;
